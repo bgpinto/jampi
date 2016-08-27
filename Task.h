57be438dfe17281a5c88e6c6539c5bc7ee41d1c0
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2016-08-27 16:35:19 -0300

    Adicionado passagem de parametros para as tarefas via tuplas.

--- Task.h
+++ Task.h
@@ -2,9 +2,17 @@
 #define _TASK_
 
 #include <memory>
+#include <tuple>
+#include <utility>
 #include "Callable.h"
 
+/*
+ *	Hipotese: parece que make_integer_sequence nao funciona/existe no g++ 4.8.4
+ *	Falso, existe, porem sobre a flag -std=c++14
+ * */
+
 namespace parallel {
+
 
 template
 <
@@ -15,35 +23,47 @@ class Task {
 	
 	// Talvez seja o caso de task gerenciar os parametros tambem
 	std::unique_ptr< Callable<ReturnType, Args...> > function;
+	
+	std::tuple<Args...> arguments;
+
+	template<std::size_t... index>
+	ReturnType unpackArguments(std::index_sequence<index...>) {
+		if (function) return (*function)(std::get<index>(arguments)...);
+
+		return ReturnType();
+	}
 
 	public:
 		
 		Task() = default;
-		~Task(){}
+		//~Task(){}
 		
 		// O objeto copiado eh destruido	
-		Task(Task& t) {
+		Task(Task& t):arguments(t.arguments) {
 		       function.reset(t.function.release());
 		 		
 		}
 
 		Task& operator = (Task& t) {
-			function.reset(t.function.release()); 
+			function.reset(t.function.release());
+		       	arguments = t.arguments;	
 			return *this;
 		}
 		
 		// Callable tricks for non-member function calls
 		template<class Fn>
-		Task(Fn f);
+		Task(Fn f, Args...args);
 
 		template<class PtrToMember, class PtrToObject>
-		Task(PtrToMember m, PtrToObject o);
+		Task(PtrToMember m, PtrToObject o, Args...args);
 
 		// here it should be a member function call
 
 		// the interface - question now is: wheter or not and how to store args;
-		ReturnType operator () (Args...args) {
-			if (function) return (*function)(args...); // virtual fowarding call to callable stuff while preserving conversions
+		ReturnType operator () () {
+			//if (function) return (*function)(args...);
+			constexpr auto size = std::tuple_size<decltype(arguments)>::value;
+			return unpackArguments(std::make_index_sequence<size>{});
 		}
 
 };
@@ -55,7 +75,7 @@ template
 
 >
 template<class Fn>
-Task<ReturnType, Args...>::Task(Fn f):function( new CallableImpl<Fn, ReturnType, Args...>(f) )
+Task<ReturnType, Args...>::Task(Fn f, Args...args):function( new CallableImpl<Fn, ReturnType, Args...>(f) ),arguments(std::tie(args...))
 {}
 
 
@@ -65,10 +85,9 @@ template
 	typename ...Args
 >
 template<class PtrToMember, class PtrToObject>
-Task<ReturnType, Args...>::Task(PtrToMember m, PtrToObject o):function( new MemberCallableImpl<PtrToMember, PtrToObject, ReturnType, Args...>(m, o) )
+Task<ReturnType, Args...>::Task(PtrToMember m, PtrToObject o, Args...args):function( new MemberCallableImpl<PtrToMember, PtrToObject, ReturnType, Args...>(m, o) ), arguments(std::tie(args...))
 {}
 
 
-};
-
+}
 #endif
