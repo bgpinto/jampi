c3eb725f6c4528742360b5b4cf7a08437bb1f6ec
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2016-09-03 02:18:19 -0300

    Implementação de Join por Contexto;
    * Implementado meio de join por contexto;
    * Verificado necessidade de mudanças importantes na classe SchedulingPolicy
    (ver SchedulingPolicy.h).
    
    Changes:
    	modified:   SchedulingPolicy.h

--- SchedulingPolicy.h
+++ SchedulingPolicy.h
@@ -8,7 +8,10 @@
 #ifndef _SCHEDULINGPOLICY_
 #define _SCHEDULINGPOLICY_
 
+#include <map>
+#include <string>
 #include "Thread.h"
+
 
 /*
  * Aqui começa o problema. Uma politica pode criar varias threads, logo essas 
@@ -32,6 +35,7 @@
 
 namespace parallel {
 // Aqui cabe SFINAE
+// deveria eu criar esta classe como um singleton?
 template
 <
 	class TASK,
@@ -42,24 +46,99 @@ class SchedulingPolicy {
 	
 	Pool workers;
 
-	public:
+	std::multimap<std::string, T<TASK>*> context_threads;
 	
-		virtual void operator()(TASK& t) {
+	using IndexIterator = typename std::multimap< std::string, T<TASK>* >::iterator;
+
+
+	/*
+	 *	A ideia do contexto funciona, porem:
+	 *
+	 *	* Uma funcao com os parametros template deve ser usada
+	 *	para criar e executar a thread, nao a classe como um todo,
+	 *	pois isso causa uma inflexibilidade no uso do escalonador
+	 *	fazendo com que o escalonador soh possa funcionar para 
+	 *	um determinado tipo de tarefa/thread e
+	 *	torna dificil o uso da classe 
+	 *
+	 *
+	 *
+	 * */
+
+
+	public:
+
+
+		/*
+		 *	Esse metodo deve ser repensado, pois:
+		 *
+		 *	Ao utilizar o conjunto de threads padrao da linguagem notou-se que,
+		 *	como as threads sao criadas no escopo desta funcao, elas acabam
+		 *	sendo destruidas antes de terem a oportunidade de sofrer join,
+		 *	o que eh um erro. Logo, as threads devem ser alocadas dinamica-
+		 *	mente. Nao sei ateh quando isto pode ser um problema.
+		 *
+		 *	Outro ponto eh que, para garantir a semantica de join em contexto,
+		 *	precisei utilizar um map. Como posso expandilo de modo ah usa-lo
+		 *	em escalonamento?? se nao puder usa-lo, como farei para que 
+		 *	isso seja eficiente?? como dar espaço para o usuario implementar
+		 *	o seu proprio algoritmo de escalonamento?
+		 *
+		 *
+		 *
+		 * */	
+		void operator()(TASK& t, std::string& context) {
 			//T<TASK> thr();
 			//(workers.*ptr)(thr);
-			//thr(t);	
+			//thr(t);
+			
+			//context_threads[context] = T<TASK>();
+
+
+
+			//T<TASK> thr;
+			
+			//T<TASK>& rthr = thr;
+
+			IndexIterator thr_ptr = context_threads.insert(std::pair<std::string, T<TASK>* >(context, new T<TASK> ));
+			//thr(t);
+			
+			thr_ptr->second->operator()(t);
+
+			//std::cout << thr_ptr->first << std::endl;
+
 		}
 
-		virtual void sync() {
+		void sync(std::string& context) {
+
+			//using IndexIterator = typename std::multimap< std::string, T<TASK>& >::iterator;
+
+			std::pair<IndexIterator, IndexIterator> joinable_threads = context_threads.equal_range(context);
+		
+
+			std::cout <<  "Joining from context === " << context  << std::endl;
+
+			for (IndexIterator itr = joinable_threads.first; itr != joinable_threads.second; itr++) {
+				itr->second->join();
+				delete itr->second;	
 			
+			}
+		
+
+			//for (auto itr = joinable_threads.first; itr != joinable_threads.second; itr++ ) itr->second.join();
+			
+			context_threads.erase(context);	
+			
+			//std::cout <<  context_threads.size()  << std::endl;
 			// isso deve mudar, pensar em uma forma melhor
 			//for (std::size_t i = 0; i < workers.size(); i++) {
 			//	workers[i].join();
 			//}
 		}
-		virtual ~SchedulingPolicy(){}
 		
-		SchedulingPolicy(){}
+		~SchedulingPolicy(){}
+		
+		SchedulingPolicy():context_threads(){}
 		SchedulingPolicy(const SchedulingPolicy&) = delete;
 		SchedulingPolicy& operator = (const SchedulingPolicy&) = delete;
 
