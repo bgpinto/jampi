ac917cee7f5e20c235afe0ecdcec3e8570c85d3b
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2016-09-05 01:21:21 -0300

    Implementação parcial do escalonador generico
    * Utiliza a nova interface uniforme de threads para implementar
    o escalonador de forma genérica. Agora, pode-se instanciar o
    escalonador para varios tipos de threads e tarefas diferentes.
    É possivel, tambem, utilizar o escalonador com um tipo fixo.
    
    NEXT:
    	1. Mudar multimap para unordered_multimap;
    	2. Implementar os padroes;
    	3. Pensar em como integrar o algoritmo de escalonamento com o
    escalonador (já parcialmente realizado -- ver SchedulingPolicy.h);
    
    Changes:
    	modified:   SchedulingPolicy.h
    	modified:   Thread.h

--- SchedulingPolicy.h
+++ SchedulingPolicy.h
@@ -9,6 +9,7 @@
 #define _SCHEDULINGPOLICY_
 
 #include <map>
+#include <type_traits>
 #include <string>
 #include "Thread.h"
 
@@ -34,113 +35,78 @@
 
 
 namespace parallel {
-// Aqui cabe SFINAE
 // deveria eu criar esta classe como um singleton?
+// Aqui acho que poderia parametrizar com o conteiner e o algoritmo de escalonamento
+// Contexto pode ser: const char* (ver func_macro.cpp)
+//
+// Devo usar unordered_map por ser mais eficiente para acesso e agrupamento de threads;
+
 template
 <
-	class TASK,
-	template <typename> class T,
-	class Pool
+	class Algorithm,
+	class THREAD = void 
 >
 class SchedulingPolicy {
 	
-	Pool workers;
+	Algorithm scheme;
 
-	std::multimap<std::string, T<TASK>*> context_threads;
+	std::multimap<std::string, parallel::ThreadInterface* > context_threads;
 	
-	using IndexIterator = typename std::multimap< std::string, T<TASK>* >::iterator;
+	using IndexIterator = typename std::multimap< std::string, parallel::ThreadInterface* >::iterator;
+	
+public:
 
 
 	/*
-	 *	A ideia do contexto funciona, porem:
+	 *	Esse metodo deve ser repensado, pois:
 	 *
-	 *	* Uma funcao com os parametros template deve ser usada
-	 *	para criar e executar a thread, nao a classe como um todo,
-	 *	pois isso causa uma inflexibilidade no uso do escalonador
-	 *	fazendo com que o escalonador soh possa funcionar para 
-	 *	um determinado tipo de tarefa/thread e
-	 *	torna dificil o uso da classe 
+	 *	Ao utilizar o conjunto de threads padrao da linguagem notou-se que,
+	 *	como as threads sao criadas no escopo desta funcao, elas acabam
+	 *	sendo destruidas antes de terem a oportunidade de sofrer join,
+	 *	o que eh um erro. Logo, as threads devem ser alocadas dinamica-
+	 *	mente. Nao sei ateh quando isto pode ser um problema.
+	 *
+	 *	Outro ponto eh que, para garantir a semantica de join em contexto,
+	 *	precisei utilizar um map. Como posso expandilo de modo ah usa-lo
+	 *	em escalonamento?? se nao puder usa-lo, como farei para que 
+	 *	isso seja eficiente?? como dar espaço para o usuario implementar
+	 *	o seu proprio algoritmo de escalonamento?
 	 *
 	 *
 	 *
 	 * */
+	template<class TASK,  class U = void >	
+	void spawn(TASK& t, std::string& context) {
 
+		typedef typename std::conditional<std::is_same<void, THREAD>::value, U, THREAD >::type T;
 
-	public:
+		T* thr = new T;
 
-
-		/*
-		 *	Esse metodo deve ser repensado, pois:
-		 *
-		 *	Ao utilizar o conjunto de threads padrao da linguagem notou-se que,
-		 *	como as threads sao criadas no escopo desta funcao, elas acabam
-		 *	sendo destruidas antes de terem a oportunidade de sofrer join,
-		 *	o que eh um erro. Logo, as threads devem ser alocadas dinamica-
-		 *	mente. Nao sei ateh quando isto pode ser um problema.
-		 *
-		 *	Outro ponto eh que, para garantir a semantica de join em contexto,
-		 *	precisei utilizar um map. Como posso expandilo de modo ah usa-lo
-		 *	em escalonamento?? se nao puder usa-lo, como farei para que 
-		 *	isso seja eficiente?? como dar espaço para o usuario implementar
-		 *	o seu proprio algoritmo de escalonamento?
-		 *
-		 *
-		 *
-		 * */	
-		void operator()(TASK& t, std::string& context) {
-			//T<TASK> thr();
-			//(workers.*ptr)(thr);
-			//thr(t);
-			
-			//context_threads[context] = T<TASK>();
-
-
-
-			//T<TASK> thr;
-			
-			//T<TASK>& rthr = thr;
-
-			IndexIterator thr_ptr = context_threads.insert(std::pair<std::string, T<TASK>* >(context, new T<TASK> ));
-			//thr(t);
-			
-			thr_ptr->second->operator()(t);
-
-			//std::cout << thr_ptr->first << std::endl;
-
-		}
-
-		void sync(std::string& context) {
-
-			//using IndexIterator = typename std::multimap< std::string, T<TASK>& >::iterator;
-
-			std::pair<IndexIterator, IndexIterator> joinable_threads = context_threads.equal_range(context);
+		context_threads.insert(std::pair<std::string, parallel::ThreadInterface* >(context, thr ));
 		
+		thr->operator()(t);	
 
-			std::cout <<  "Joining from context === " << context  << std::endl;
+	}
 
-			for (IndexIterator itr = joinable_threads.first; itr != joinable_threads.second; itr++) {
-				itr->second->join();
-				delete itr->second;	
-			
-			}
+	void sync(std::string& context) {
+
+		std::pair<IndexIterator, IndexIterator> joinable_threads = context_threads.equal_range(context);
+
+		for (IndexIterator itr = joinable_threads.first; itr != joinable_threads.second; itr++) {
+			itr->second->join();
+			delete itr->second;	
 		
-
-			//for (auto itr = joinable_threads.first; itr != joinable_threads.second; itr++ ) itr->second.join();
-			
-			context_threads.erase(context);	
-			
-			//std::cout <<  context_threads.size()  << std::endl;
-			// isso deve mudar, pensar em uma forma melhor
-			//for (std::size_t i = 0; i < workers.size(); i++) {
-			//	workers[i].join();
-			//}
 		}
 		
-		~SchedulingPolicy(){}
+		context_threads.erase(context);	
 		
-		SchedulingPolicy():context_threads(){}
-		SchedulingPolicy(const SchedulingPolicy&) = delete;
-		SchedulingPolicy& operator = (const SchedulingPolicy&) = delete;
+	}
+	
+	~SchedulingPolicy(){}
+	
+	SchedulingPolicy():context_threads(){}
+	SchedulingPolicy(const SchedulingPolicy&) = delete;
+	SchedulingPolicy& operator = (const SchedulingPolicy&) = delete;
 
 };
 
--- Thread.h
+++ Thread.h
@@ -5,21 +5,17 @@
 
 namespace parallel {
 
-// The ninja
 class ThreadInterface {
 public:
-	using TASK = parallel::Task<>;
 	virtual ~ThreadInterface(){}
-	virtual void operator()(TASK&) = 0;
 	virtual void join() = 0;
 };
 
-
 template<class TASK>
 class Thread : public virtual ThreadInterface {
-	public:
-		virtual void operator()(TASK&) = 0;
-		virtual ~Thread(){}
+public:
+	virtual void operator()(TASK&) = 0;
+	virtual ~Thread(){}
 
 };
 	
